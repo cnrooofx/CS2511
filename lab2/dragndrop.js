61560758771a5befd8456edc1031d11097c9e91b
Conor <conor@conorfox.com>
2021-02-25 09:24:10 +0000

    Add vertical and horizontal offsets to drag

--- lab2/dragndrop.js
+++ lab2/dragndrop.js
@@ -1,4 +1,5 @@
 var myDiv = null;
+var hOffset, vOffset = 0;
 
 window.addEventListener('DOMContentLoaded', setupEvents, false);
 
@@ -7,15 +8,17 @@ function setupEvents() {
     myDiv.addEventListener('mousedown', startDrag, false);
 }
 
-function startDrag() {
+function startDrag(event) {
     document.addEventListener('mousemove', dragDiv, false);
     document.addEventListener('mouseup', stopDrag, false);
+    hOffset = parseInt(event.clientX) - parseInt(myDiv.style.left);
+    vOffset = parseInt(event.clientY) - parseInt(myDiv.style.top);
     return false;
 }
 
 function dragDiv(event) {
-    myDiv.style.left = parseInt(event.clientX) + 'px';
-    myDiv.style.top = parseInt(event.clientY) + 'px';
+    myDiv.style.left = (parseInt(event.clientX) - hOffset) + 'px';
+    myDiv.style.top = (parseInt(event.clientY) - vOffset) + 'px';
     return false;
 }
 
