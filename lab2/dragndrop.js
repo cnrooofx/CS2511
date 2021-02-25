a9f6579e2ca80dfb3676261c35625ace38e6f54b
Conor <conor@conorfox.com>
2021-02-25 09:40:49 +0000

    Change zIndex to avoid overlapping

--- lab2/dragndrop.js
+++ lab2/dragndrop.js
@@ -5,7 +5,9 @@ window.addEventListener('DOMContentLoaded', setupEvents, false);
 
 function setupEvents() {
     var allDivs = document.getElementsByTagName('div')
+
     for (var i = 0; i < allDivs.length; i++) {
+        allDivs[i].style.zIndex = 0;
         allDivs[i].addEventListener('mousedown', startDrag, false);
     }
 }
@@ -14,10 +16,10 @@ function startDrag(event) {
     document.addEventListener('mousemove', dragDiv, false);
     document.addEventListener('mouseup', stopDrag, false);
     selectedDiv = event.target;
+    selectedDiv.style.zIndex = 1;
 
     // hOffset = parseInt(event.clientX) - parseInt(selectedDiv.style.left);
     // vOffset = parseInt(event.clientY) - parseInt(selectedDiv.style.top);
-
     return false;
 }
 
@@ -28,6 +30,7 @@ function dragDiv(event) {
 }
 
 function stopDrag() {
+    selectedDiv.style.zIndex = 0;
     document.removeEventListener('mousemove', dragDiv, false);
     document.removeEventListener('mouseup', stopDrag, false);
 }
