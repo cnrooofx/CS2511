5202f25367cc7e096c5d007455700006246ce730
Conor Fox <conor@conorfox.com>
2021-03-08 11:40:19 +0000

    Lab3 (#2)
    * Add vertical and horizontal offsets to drag
    
    * Multiple divs
    
    * Change zIndex to avoid overlapping
    
    * Offset for multiple divs
    
    * Update README.md

--- lab2/dragndrop.js
+++ lab2/dragndrop.js
@@ -1,25 +1,50 @@
-var myDiv = null;
+var selectedDiv = null;
+var hOffset, vOffset = 0;
 
 window.addEventListener('DOMContentLoaded', setupEvents, false);
 
 function setupEvents() {
-    myDiv = document.getElementById('div0');
-    myDiv.addEventListener('mousedown', startDrag, false);
+    var allDivs = document.getElementsByTagName('div')
+
+    for (var i = 0; i < allDivs.length; i++) {
+        allDivs[i].style.zIndex = 0;
+        allDivs[i].addEventListener('mousedown', startDrag, false);
+    }
 }
 
-function startDrag() {
+function startDrag(event) {
     document.addEventListener('mousemove', dragDiv, false);
     document.addEventListener('mouseup', stopDrag, false);
+    selectedDiv = event.target;
+    selectedDiv.style.zIndex = 1;
+
+    var left = parseInt(selectedDiv.style.left);
+    var top = parseInt(selectedDiv.style.top);
+
+    console.log(event);
+    console.log(top);
+
+    if (isNaN(left)) {
+        hOffset = 0
+    } else {
+        hOffset = (parseInt(event.clientX) - left);
+    }
+    if (isNaN(top)) {
+        vOffset = 0
+    } else {
+        vOffset = (parseInt(event.clientY) - top);
+    }
     return false;
 }
 
 function dragDiv(event) {
-    myDiv.style.left = parseInt(event.clientX) + 'px';
-    myDiv.style.top = parseInt(event.clientY) + 'px';
+    selectedDiv.style.left = (parseInt(event.clientX) - hOffset) + 'px';
+    selectedDiv.style.top = (parseInt(event.clientY) - vOffset) + 'px';
     return false;
 }
 
 function stopDrag() {
+    selectedDiv.style.zIndex = 0;
     document.removeEventListener('mousemove', dragDiv, false);
     document.removeEventListener('mouseup', stopDrag, false);
 }
