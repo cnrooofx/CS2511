92dae5f8a6807a4cdb8adb412a1b9a1b2007e66b
Conor Fox <conor@conorfox.com>
2021-03-25 09:37:10 +0000

    Multiple sliders

--- lab7/libraries.js
+++ lab7/libraries.js
@@ -2,13 +2,13 @@
 window.addEventListener("DOMContentLoaded", initialise, false);
 
 function initialise() {
-    $("#slider").slider({value:[50]});
+    $("#slider").slider({values:[20, 80]});
 
     $("#slider").on(
         {slide:
             function() {
-                var setting = $("#slider").slider("option", "value");
-                document.getElementById("textBox").value = setting;
+                var setting = $("#slider").slider("option", "values");
+                document.getElementById("textBox").value = setting[0] + ", " + setting[1];
             }
         }
     );
